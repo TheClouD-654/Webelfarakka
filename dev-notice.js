1f21e39d9c5e1b31246da295fb12406cdab08369
TheClouD-654 <arunangshucloud@gmail.com>
2026-03-03 00:15:25 +0530

    commit

--- dev-notice.js
+++ dev-notice.js
@@ -1,5 +1,5 @@
 ﻿(() => {
-  const SHOW_DEV_NOTICE = true;
+  const SHOW_DEV_NOTICE = false;
   if (!SHOW_DEV_NOTICE) return;
 /// <reference path="./dev-notice.js" />
   const style = document.createElement('style');
